8a6eec85cee697ae072bdc309734d0cd55f8d556
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2018-01-09 18:47:35 -0200

    Change phaser.js to phaser.min.js

---
+++ client/index.js
@@ -0,0 +1,11 @@
+import Game from './states/Game'
+
+class App extends Phaser.Game {
+  constructor () {
+    super(800, 600, Phaser.AUTO)
+    this.state.add('Game', Game)
+    this.state.start('Game')
+  }
+}
+
+const SimpleGame = new App()

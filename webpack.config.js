83f829eca51ccd2eb6f1302336ef810701e9bc42
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2018-01-09 18:25:55 -0200

    Add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+const path = require('path')
+
+const config = {
+  target: 'web',
+  entry: './client/index.js',
+  watch: true,
+  output: {
+    path: path.resolve(__dirname, 'dist/client'),
+    filename: 'index.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader']
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    }),
+    new HtmlWebpackPlugin({ template: './client/index.html' })
+  ]
+}
+
+module.exports = config

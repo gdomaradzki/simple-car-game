ceec2b00219c2033bae2f93d5a688ad7acafbbfd
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2018-01-10 12:47:26 -0200

    Add player file

---
+++ client/state/player/index.js
@@ -0,0 +1,100 @@
+import createPlayer from './createPlayer'
+import { isDown } from '../utils'
+
+export default function (x, y, game, socket) {
+  const player = {
+    socket,
+    sprite: createPlayer(x, y, game),
+    playerName: null,
+    speed: 0,
+    speedText: null,
+    drive (game) {
+      /*
+      Most of the driving logic was written by Daniel Wuggenig
+      https://www.anexia-it.com/blog/en/introduction-to-the-phaser-framework/
+      I decided to use it since this is supposed to be an introduction to multiplayer
+      online car game, his driving solution is simple and clean and fits perfectly
+      */
+
+      const KEYS = {
+        W: Phaser.Keyboard.W,
+        S: Phaser.Keyboard.S,
+        A: Phaser.Keyboard.A,
+        D: Phaser.Keyboard.D
+      }
+
+      // Drive forward if W is pressed down
+      if (isDown(game, KEYS.W) && this.speed <= 700) {
+        this.speed += 10
+      } else {
+        if (this.speed >= 10) {
+          this.speed -= 10
+        }
+      }
+
+      // Drive backwards if S is pressed down
+      if (isDown(game, KEYS.S) && this.speed >= -200) {
+        this.speed -= 5
+      } else {
+        if (this.speed <= -5) {
+          this.speed += 5
+        }
+      }
+
+      // Steers the car
+      if (isDown(game, KEYS.A)) {
+        this.sprite.body.angularVelocity = -5 * (this.speed / 1000)
+      } else if (isDown(game, KEYS.D)) {
+        this.sprite.body.angularVelocity = 5 * (this.speed / 1000)
+      } else {
+        this.sprite.body.angularVelocity = 0
+      }
+
+      this.sprite.body.velocity.x = this.speed * Math.cos((this.sprite.body.angle - 360) * 0.01745)
+      this.sprite.body.velocity.y = this.speed * Math.sin((this.sprite.body.angle - 360) * 0.01745)
+
+      // Brings the player's sprite to top
+      game.world.bringToTop(this.sprite)
+
+      this.updatePlayerName()
+      this.updatePlayerStatusText('speed', this.sprite.body.x - 57, this.sprite.body.y - 18, this.speedText)
+
+      // Emit the 'move-player' event, updating the player's data on the server
+      socket.emit('move-player', {
+        x: this.sprite.body.x,
+        y: this.sprite.body.y,
+        playerName: {
+          name: this.playerName.text,
+          x: this.playerName.x,
+          y: this.playerName.y
+        },
+        speed: {
+          value: this.speed,
+          x: this.speedText.x,
+          y: this.speedText.y
+        }
+      })
+    },
+    updatePlayerName (name, x, y) {
+      // Updates the player's name text and position
+      this.playerName.text = String(name)
+      this.playerName.x = x
+      this.playerName.y = y
+      // Bring the player's name to top
+      game.world.bringToTop(this.playerName)
+    },
+    updatePlayerStatusText (status, x, y, text) {
+      // Capitalize the status text
+      const capitalizedStatus = status[0].toUpperCase() + status.substring(1)
+      let newText = ''
+      // Set the speed text to either 0 or the current speed
+      this[status] < 0 ? this.newText = 0 : this.newText = this[status]
+      // Updates the text position and string
+      text.x = x
+      text.y = y
+      text.text = `${capitalizedStatus}: ${parseInt(this.newText)}`
+      game.world.bringToTop(text)
+    }
+  }
+  return player
+}

fb69db231a8e105ca7d4fab7925341e8857cd8e4
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2018-01-10 14:50:40 -0200

    Change IP to heroku server

--- client/state/Game.js
+++ client/state/Game.js
@@ -7,7 +7,7 @@ import newPlayer from './sockets/newPlayer'
 import updatePlayers from './sockets/updatePlayers'
 import playerMovementInterpolation from './predictions/playerMovementInterpolation'
 
-const SERVER_IP = 'http://localhost:8000'
+const SERVER_IP = 'https://simple-car-game.herokuapp.com/'
 let socket = null
 let otherPlayers = {}
 

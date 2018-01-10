947e2b4bfedbffee77b81d7270f28cccab8cb52e
Gustavo Domaradzki <gutzedomaradzki@gmail.com>
2018-01-10 14:22:16 -0200

    Add window width and height as variables

--- client/config/index.js
+++ client/config/index.js
@@ -1,2 +1,4 @@
+export const WINDOW_WIDTH = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
+export const WINDOW_HEIGHT = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight
 export const WORLD_SIZE = { width: 1600, height: 1200 }
 export const ASSETS_URL = '../assets'
